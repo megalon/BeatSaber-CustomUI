8cef44bf493e2d6373b7ff6e7f40ee12616c7c5e
andruzzzhka <andruzzzhka@yandex.ru>
2018-11-26 23:15:17 +0700

    Added BeatSaberUI with extensions

---
+++ BeatSaberUI.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using VRUI;
+using Image = UnityEngine.UI.Image;
+
+namespace BeatSaberCustomUI
+{
+    public class BeatSaberUI : MonoBehaviour
+    {
+        private static Button _backButtonInstance;
+        private static GameObject _loadingIndicatorInstance;
+
+        public static bool initialized = false;
+
+        public static Button CreateUIButton(RectTransform parent, string buttonTemplate, Vector2 anchoredPosition, Vector2 sizeDelta, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = Instantiate(Resources.FindObjectsOfTypeAll<Button>().Last(x => (x.name == buttonTemplate)), parent, false);
+            DestroyImmediate(btn.GetComponent<SignalOnUIButtonClick>());
+            btn.onClick = new Button.ButtonClickedEvent();
+            if (onClick != null)
+                btn.onClick.AddListener(onClick);
+            btn.name = "CustomUIButton";
+
+            (btn.transform as RectTransform).anchorMin = new Vector2(0.5f, 0.5f);
+            (btn.transform as RectTransform).anchorMax = new Vector2(0.5f, 0.5f);
+            (btn.transform as RectTransform).anchoredPosition = anchoredPosition;
+            (btn.transform as RectTransform).sizeDelta = sizeDelta;
+
+            btn.SetButtonText(buttonText);
+            if (icon != null)
+                btn.SetButtonIcon(icon);
+
+            return btn;
+        }
+
+        public static Button CreateUIButton(RectTransform parent, string buttonTemplate, Vector2 anchoredPosition, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = Instantiate(Resources.FindObjectsOfTypeAll<Button>().Last(x => (x.name == buttonTemplate)), parent, false);
+            DestroyImmediate(btn.GetComponent<SignalOnUIButtonClick>());
+            btn.onClick = new Button.ButtonClickedEvent();
+            if (onClick != null)
+                btn.onClick.AddListener(onClick);
+            btn.name = "CustomUIButton";
+
+            (btn.transform as RectTransform).anchorMin = new Vector2(0.5f, 0.5f);
+            (btn.transform as RectTransform).anchorMax = new Vector2(0.5f, 0.5f);
+            (btn.transform as RectTransform).anchoredPosition = anchoredPosition;
+
+            btn.SetButtonText(buttonText);
+            if (icon != null)
+                btn.SetButtonIcon(icon);
+
+            return btn;
+        }
+        
+        public static Button CreateUIButton(RectTransform parent, string buttonTemplate, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = Instantiate(Resources.FindObjectsOfTypeAll<Button>().Last(x => (x.name == buttonTemplate)), parent, false);
+            DestroyImmediate(btn.GetComponent<SignalOnUIButtonClick>());
+            btn.onClick = new Button.ButtonClickedEvent();
+            if (onClick != null)
+                btn.onClick.AddListener(onClick);
+            btn.name = "CustomUIButton";
+
+            (btn.transform as RectTransform).anchorMin = new Vector2(0.5f, 0.5f);
+            (btn.transform as RectTransform).anchorMax = new Vector2(0.5f, 0.5f);
+            btn.SetButtonText(buttonText);
+            if (icon != null)
+                btn.SetButtonIcon(icon);
+
+            return btn;
+        }
+
+        public static Button CreateBackButton(RectTransform parent, UnityAction onClick = null)
+        {
+            if (_backButtonInstance == null)
+            {
+                try
+                {
+                    _backButtonInstance = Resources.FindObjectsOfTypeAll<Button>().First(x => (x.name == "BackArrowButton"));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            
+            Button btn = Instantiate(_backButtonInstance, parent, false);
+            DestroyImmediate(btn.GetComponent<SignalOnUIButtonClick>());
+            btn.onClick = new Button.ButtonClickedEvent();
+            if (onClick != null)
+                btn.onClick.AddListener(onClick);
+            btn.name = "CustomUIButton";
+            
+            return btn;
+        }
+
+        public static T CreateViewController<T>() where T : VRUIViewController
+        {
+            T vc = new GameObject("CustomViewController").AddComponent<T>();
+
+            vc.rectTransform.anchorMin = new Vector2(0f, 0f);
+            vc.rectTransform.anchorMax = new Vector2(1f, 1f);
+            vc.rectTransform.sizeDelta = new Vector2(0f, 0f);
+            vc.rectTransform.anchoredPosition = new Vector2(0f, 0f);
+            
+            return vc;
+        }
+
+        public static GameObject CreateLoadingSpinner(Transform parent)
+        {
+            if (_loadingIndicatorInstance == null)
+            {
+                try
+                {
+                    _loadingIndicatorInstance = Resources.FindObjectsOfTypeAll<GameObject>().Where(x => x.name == "LoadingIndicator").First();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            GameObject loadingSpinner = Instantiate(_loadingIndicatorInstance, parent, false);
+            loadingSpinner.name = "CustomUILoadingSpinner";
+
+            return loadingSpinner;
+        }
+
+        public static TextMeshProUGUI CreateText(RectTransform parent, string text, Vector2 anchoredPosition)
+        {
+            TextMeshProUGUI textMesh = new GameObject("CustomUIText").AddComponent<TextMeshProUGUI>();
+            textMesh.rectTransform.SetParent(parent, false);
+            textMesh.text = text;
+            textMesh.fontSize = 4;
+            textMesh.color = Color.white;
+            textMesh.font = Resources.Load<TMP_FontAsset>("Teko-Medium SDF No Glow");
+            textMesh.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            textMesh.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            textMesh.rectTransform.sizeDelta = new Vector2(60f, 10f);
+            textMesh.rectTransform.anchoredPosition = anchoredPosition;
+
+            return textMesh;
+        }
+
+        public static TextMeshProUGUI CreateText(RectTransform parent, string text, Vector2 anchoredPosition, Vector2 sizeDelta)
+        {
+            TextMeshProUGUI textMesh = new GameObject("CustomUIText").AddComponent<TextMeshProUGUI>();
+            textMesh.rectTransform.SetParent(parent, false);
+            textMesh.text = text;
+            textMesh.fontSize = 4;
+            textMesh.color = Color.white;
+            textMesh.font = Resources.Load<TMP_FontAsset>("Teko-Medium SDF No Glow");
+            textMesh.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            textMesh.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            textMesh.rectTransform.sizeDelta = sizeDelta;
+            textMesh.rectTransform.anchoredPosition = anchoredPosition;
+
+            return textMesh;
+        }
+    }
+}
---
+++ BeatSaberUIExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using VRUI;
+
+namespace BeatSaberCustomUI
+{
+    public static class BeatSaberUIExtensions
+    {
+        #region Button Extensions
+        public static void SetButtonText(this Button _button, string _text)
+        {
+            if (_button.GetComponentInChildren<TextMeshProUGUI>() != null)
+                _button.GetComponentInChildren<TextMeshProUGUI>().text = _text;
+        }
+
+        public static void SetButtonTextSize(this Button _button, float _fontSize)
+        {
+            if (_button.GetComponentInChildren<TextMeshProUGUI>() != null)
+            {
+                _button.GetComponentInChildren<TextMeshProUGUI>().fontSize = _fontSize;
+            }
+        }
+
+        public static void ToggleWordWrapping(this Button _button, bool enableWordWrapping)
+        {
+            if (_button.GetComponentInChildren<TextMeshProUGUI>() != null)
+            {
+                _button.GetComponentInChildren<TextMeshProUGUI>().enableWordWrapping = enableWordWrapping;
+            }
+        } 
+
+        public static void SetButtonIcon(this Button _button, Sprite _icon)
+        {
+            if (_button.GetComponentsInChildren<Image>().Count() > 1)
+                _button.GetComponentsInChildren<Image>().First(x => x.name == "Icon").sprite = _icon;
+        }
+
+        public static void SetButtonBackground(this Button _button, Sprite _background)
+        {
+            if (_button.GetComponentsInChildren<Image>().Count() > 0)
+                _button.GetComponentsInChildren<Image>()[0].sprite = _background;
+        }
+        #endregion
+
+        #region ViewController Extensions
+
+        public static Button CreateUIButton(this VRUIViewController parent, string buttonTemplate)
+        {
+            Button btn = BeatSaberUI.CreateUIButton(parent.rectTransform, buttonTemplate);
+            return btn;
+        }
+
+        public static Button CreateUIButton(this VRUIViewController parent, string buttonTemplate, Vector2 anchoredPosition, Vector2 sizeDelta, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = BeatSaberUI.CreateUIButton(parent.rectTransform, buttonTemplate, anchoredPosition, sizeDelta, onClick, buttonText, icon);
+            return btn;
+        }
+
+        public static Button CreateUIButton(this VRUIViewController parent, string buttonTemplate, Vector2 anchoredPosition, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = BeatSaberUI.CreateUIButton(parent.rectTransform, buttonTemplate, anchoredPosition, onClick, buttonText, icon);
+            return btn;
+        }
+
+        public static Button CreateUIButton(this VRUIViewController parent, string buttonTemplate, UnityAction onClick = null, string buttonText = "BUTTON", Sprite icon = null)
+        {
+            Button btn = BeatSaberUI.CreateUIButton(parent.rectTransform, buttonTemplate, onClick, buttonText, icon);
+            return btn;
+        }
+
+        public static Button CreateBackButton(this VRUIViewController parent)
+        {
+            Button btn = BeatSaberUI.CreateBackButton(parent.rectTransform);
+            return btn;
+        }
+
+        public static GameObject CreateLoadingSpinner(this VRUIViewController parent)
+        {
+            GameObject loadingSpinner = BeatSaberUI.CreateLoadingSpinner(parent.rectTransform);
+            return loadingSpinner;
+        }
+
+        public static TextMeshProUGUI CreateText(this VRUIViewController parent, string text, Vector2 anchoredPosition, Vector2 sizeDelta)
+        {
+            TextMeshProUGUI textMesh = BeatSaberUI.CreateText(parent.rectTransform, text, anchoredPosition, sizeDelta);
+            return textMesh;
+        }
+
+        public static TextMeshProUGUI CreateText(this VRUIViewController parent, string text, Vector2 anchoredPosition)
+        {
+            TextMeshProUGUI textMesh = BeatSaberUI.CreateText(parent.rectTransform, text, anchoredPosition);
+            return textMesh;
+        }
+        #endregion
+
+    }
+}
